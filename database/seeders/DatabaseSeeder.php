11ee25bcd4fa1a97652e0eaed54cd5cad8e85b22
pra12333 <praveen60fernando@gmail.com>
2024-07-27 13:05:28 +0900

    second commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        DB::table('products')->insert([
+            'name' => Str::random(10),  // Generate a random name
+            'price' => rand(100, 1000),  // Generate a random price
+            'color' => Str::random(7),  // Generate a random color
         ]);
     }
 }
